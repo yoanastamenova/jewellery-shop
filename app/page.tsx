68d5bce8250d4d813cf5cdb8c9af59725e136f00
Yoana Stamenova <yoana.stamenova@maibornwolff.de>
2025-09-10 15:21:51 +0200

    feat: add styling, first section complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import { stripe } from "@/lib/stripe";
-import styles from "./page.module.css";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Image from "next/image";
+import { Carousel } from "@/components/carousel";
 
 export default async function Home() {
   const products = await stripe.products.list({
@@ -12,17 +12,34 @@ export default async function Home() {
 
   return (
     <div>
-      <section>
-        <div>
-          <div>
-            <h2>Welcome to Luxotica</h2>
-            <p> Discover how lux meets reality and perfection</p>
-            <Button asChild variant="default">
-              <Link href="/products">Browse All Products</Link>
+      <section className="rounded bg-neutral-100 py-8 sm:py-12">
+        <div className="mx-auto grid grid-cols-1 gap-8 px-8 sm:px-16 md:grid-cols-2 md:items-center">
+          <div className="max-w-md space-y-4">
+            <h2 className="text-3xl font-bold tracking-tight md:text-4xl">
+            Welcome to Luxotica</h2>
+            <p className="text-neutral-600"> Discover how lux meets reality and perfection</p>
+            <Button 
+            asChild 
+            variant="default"
+            className="inline-flex items-center justify-center rounded-full px-6 py-3 bg-black text-white"
+            >
+              <Link 
+              href="/products"
+              className="inline-flex items-center justify-center rounded-full px-6 py-3"
+              >Browse All Products</Link>
             </Button>
           </div>
-          <Image alt="Banner Image" width={450} height={450} src={products.data[0].images[0]}></Image>
+          <Image 
+          alt="Banner Image" 
+          width={450} 
+          height={450} 
+          src={products.data[3].images[0]}
+          className="rounded"
+          ></Image>
         </div>
+        </section>
+        <section className="py-8">
+          <Carousel />
         </section>
     </div>
   );
---
+++ components/carousel.tsx
@@ -0,0 +1,6 @@
+
+export const Carousel = () => {
+  return (
+    <div>carousel</div>
+  )
+}
