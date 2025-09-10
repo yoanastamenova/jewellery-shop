33d1e52759acd5d43e4313da1a89db088719f76a
Yoana Stamenova <yoana.stamenova@maibornwolff.de>
2025-09-10 15:47:32 +0200

    feat: home complete

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default async function Home() {
         </div>
         </section>
         <section className="py-8">
-          <Carousel />
+          <Carousel products={products.data}/>
         </section>
     </div>
   );
