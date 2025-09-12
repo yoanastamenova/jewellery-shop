18330966ec9fca6139fb20c9507fc4cb1e28c623
Yoana Stamenova <yoana.stamenova@maibornwolff.de>
2025-09-12 11:07:13 +0200

    feat: add logo

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export const Navbar = () => {
@@ -5,7 +6,7 @@ export const Navbar = () => {
         <nav className="sticky top-0 z-50 bg-white shadow">
             <div className="container mx-auto flex items-center justify-between px-4 py-4">
               <Link href="/" className="hover:text-blue-600"> 
-              My Ecommerce
+                <Image src="/logo2.png" height={80} width={80} alt="logo"/>
               </Link>
             <div className="hidden md:flex space-x-6">
                 <Link href="/">Home</Link>
