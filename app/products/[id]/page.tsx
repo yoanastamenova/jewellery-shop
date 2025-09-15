3632a7986216add692a60fc619327977762f0744
yoanastamenova <yoana.stamenovaa@gmail.com>
2025-09-15 11:42:44 +0200

    feat: add page for product details

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,14 @@
+import { ProductDetail } from "@/components/product-detail"
+import { stripe } from "@/lib/stripe"
+
+export default async function ProductPage({
+    params, 
+    }: {
+        params: {id: string}
+    }) {
+    const product = await stripe.products.retrieve(params.id, {
+        expand: ["default_price"],
+
+    })
+  return <ProductDetail product={product}/>
+}
---
+++ components/product-detail.tsx
@@ -0,0 +1,48 @@
+import Stripe from "stripe";
+import Image from "next/image";
+import { Button } from "./ui/button";
+interface Props {
+  product: Stripe.Product;
+}
+
+export const ProductDetail = ({ product }: Props) => {
+  const price = product.default_price as Stripe.Price;
+
+  return (
+    <div className="container mx-auto px-4 py-8 flex flex-col md:flex-row gap-8 items-center">
+      {product.images && product.images[0] && (
+        <div className="relative h-60 w-full md:w-1/2 rounded-lg overflow-hidden">
+          <Image
+            alt={product.name}
+            src={product.images[0]}
+            layout="fill"
+            objectFit="cover"
+            className="transition duration-300 hover:opacity-90"
+          />
+        </div>
+      )}
+
+      <div className="md:w-1/2">
+        <h1 className="text-3xl font-bold mb-4"> {product.name} </h1>
+        {product.description && (
+        <p className="text-gray-700 mb-4"> {product.description} </p>
+        )}
+        {price && price.unit_amount && (
+          <p className="text-lg font-semibold text-gray-900">
+            {new Intl.NumberFormat("de-DE", {
+              style: "currency",
+              currency: "EUR",
+              minimumFractionDigits: 0,
+            }).format(price.unit_amount / 100)}
+          </p>
+        )}
+
+        <div className="flex items center space-x-4">
+            <Button variant="outline"> - </Button>
+            <span className="text-lg font-semibold"> 0 </span>
+            <Button variant="outline"> + </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
