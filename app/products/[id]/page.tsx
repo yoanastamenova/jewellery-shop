b1577d4c63c34eeb87ae2d405bde020c0e57a35d
yoanastamenova <yoana.stamenovaa@gmail.com>
2025-09-15 15:00:23 +0200

    feat: complete product-details option and addToCart function

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -4,11 +4,13 @@ import { stripe } from "@/lib/stripe"
 export default async function ProductPage({
     params, 
     }: {
-        params: {id: string}
+        params: Promise<{id: string}>
     }) {
-    const product = await stripe.products.retrieve(params.id, {
+    const { id } = await params;
+    const product = await stripe.products.retrieve(id, {
         expand: ["default_price"],
-
     })
-  return <ProductDetail product={product}/>
+        const plainProduct = JSON.parse(JSON.stringify(product));
+
+  return <ProductDetail product={plainProduct} />
 }
